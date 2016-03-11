ed0e4616b8ee8279a83f50d251fecffd9ec791ce
Dries Gotink <dries.gotink@student.ucll.be>
2016-03-11 14:05:29 +0100

    data.js verbeterd.

--- src/main/webapp/js/Data.js
+++ src/main/webapp/js/Data.js
@@ -1,8 +1,25 @@
-//Puts all the parameters in an array
-function merge(){
-    var result = [];
+var arr = {};
+
+function add(){
     for(var i = 0; i < arguments.length; i++){
-        result[i] = arguments[i];
+        arr[arguments[i].Name] =  arguments[i];
     }
-    return result;
+    return getArr();
+}
+
+function remove(){
+    for(var i = 0; i < arguments.length; i++){
+        delete arr[arguments[i].Name];
+    }
+    return getArr();
+}
+
+function getArr(){
+    var output = [];
+    for(key in arr){
+        if(arr.hasOwnProperty(key)){
+            output.push(arr[key]);
+        } 
+    }
+    return output;
 }
