f9f0e5257be67fa6d57bea740f229e59902f2d5f
Dries Gotink <dries.gotink@student.ucll.be>
2016-03-11 10:51:11 +0100

    LineChart function in apart bestand gestoken -D

---
+++ src/main/webapp/js/LineChart.js
@@ -0,0 +1,88 @@
+//go to https://bost.ocks.org/mike/chart/ for a detailed explanation
+function LineChart(){
+    //Width & height
+    var svgWidth = 800;
+    var svgHeight = 550;
+    var padding = 25;
+    
+    function chart(selection){
+        selection.each(function(data) { 
+         //Make the dataset and map (map for drawing, dataset for the names)
+        var dataset = data;
+        var map = dataset.map(function (d) { return d.Data; });
+                
+        //Variables needed for scale
+        var xExtents = d3.extent(d3.merge(map), function (d) { return d.x; });
+        var yExtents = d3.extent(d3.merge(map), function (d) { return d.y; });
+        
+		
+        //Scales
+        var xScale = d3.scale.linear()
+            .domain([xExtents[0], xExtents[1]])
+            .range([padding, svgWidth - padding]);
+							 
+        var yScale = d3.scale.linear()
+            .domain([yExtents[0], yExtents[1]])
+            .range([svgHeight - padding, padding]);
+							 
+        //Axis
+        var xAxis = d3.svg.axis()
+            .scale(xScale)
+            .orient("bottom")
+            .ticks(8);
+						  
+        var yAxis = d3.svg.axis()
+            .scale(yScale)
+            .orient("left")
+            .ticks(6);
+									 
+        //Svg						 
+        var svg = d3.select(this)
+            .append("svg")
+            .attr("width", svgWidth)
+            .attr("height", svgHeight)
+            .attr("class", "svgbox");
+					
+        //Axis functions
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(0, " + (svgHeight - padding) + ")")
+            .call(xAxis);
+		   
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + padding + ",0)")
+            .call(yAxis);
+
+        var pathContainers = svg.selectAll('g.line')
+            .data(map);
+	
+        pathContainers.enter().append('g')
+            .attr('class', 'line');
+	
+        //draw the lines    
+        pathContainers.selectAll('path')
+            .data(function(d) { return [d]; }) // continues the data from the pathContainer
+            .enter().append('path')
+            .attr('d', d3.svg.line()
+                .x(function (d) { return xScale(d.x); })
+                .y(function (d) { return yScale(d.y); })
+                .interpolate("monotone")
+            );
+		
+        // add circles
+        pathContainers.selectAll('circle')
+            .data(function(d) { return d; })
+            .enter().append('circle')
+            .attr('cx', function (d) { return xScale(d.x); })
+            .attr('cy', function (d) { return yScale(d.y); })
+            .attr('r', 5); 
+    
+        });
+        
+    }
+    
+    return chart;
+    
+}            
+                
