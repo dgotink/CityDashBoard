dcb787440bab6e776c065bffba6515b796c52d56
Dries Gotink <dries.gotink@student.ucll.be>
2016-07-07 17:04:37 +0200

    seperated the introtext so that in the future it is easily changeable

--- scripts/graph_context.js
+++ scripts/graph_context.js
@@ -80,20 +80,7 @@ function graph_context(){
             
                 introgroup.append('text')
                     .attr('x', padding.left)
-                    .text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n\
-                    Nullam iaculis libero tellus, eget tincidunt purus auctor nec. \n\
-                    Interdum et malesuada fames ac ante ipsum primis in faucibus. \n\
-                    Sed pharetra ex egestas maximus sagittis. \n\
-                    In vitae tincidunt elit. \n\
-                    Mauris mi ante, pretium et nisi vel, lobortis mattis arcu.\n\
-                    Ut bibendum mauris at magna dignissim, id elementum nibh bibendum. \n\
-                    Quisque sit amet nunc lectus. Aliquam tincidunt risus non cursus lobortis. \n\
-                    Nunc augue mi, rhoncus in lectus eget, faucibus lobortis libero. \n\
-                    Suspendisse vitae purus eget nulla molestie efficitur. \n\
-                    Morbi ante massa, vulputate vel malesuada in, dictum at elit. Pellentesque aliquet vitae urna at accumsan. \n\
-                    Mauris maximus, ipsum ac porttitor mattis, quam mi hendrerit ligula, non pellentesque enim mi dapibus ante. \n\
-                    Vestibulum nisl lectus, fermentum in auctor at, imperdiet nec nulla. \n\
-                    Fusce tellus libero, venenatis vitae neque eu, vehicula placerat tortor. Sed ultrices aliquam risus sed sodales.')
+                    .text(getIntroText())
                     .style('font-size', 16)
                     .style('font-family', 'Helvetica Neue,Helvetica,Arial,sans-serif;')
                     .style('fill', 'white')
---
+++ scripts/introtext.js
@@ -0,0 +1,16 @@
+function getIntroText(){
+    return 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n\
+                    Nullam iaculis libero tellus, eget tincidunt purus auctor nec. \n\
+                    Interdum et malesuada fames ac ante ipsum primis in faucibus. \n\
+                    Sed pharetra ex egestas maximus sagittis. \n\
+                    In vitae tincidunt elit. \n\
+                    Mauris mi ante, pretium et nisi vel, lobortis mattis arcu.\n\
+                    Ut bibendum mauris at magna dignissim, id elementum nibh bibendum. \n\
+                    Quisque sit amet nunc lectus. Aliquam tincidunt risus non cursus lobortis. \n\
+                    Nunc augue mi, rhoncus in lectus eget, faucibus lobortis libero. \n\
+                    Suspendisse vitae purus eget nulla molestie efficitur. \n\
+                    Morbi ante massa, vulputate vel malesuada in, dictum at elit. Pellentesque aliquet vitae urna at accumsan. \n\
+                    Mauris maximus, ipsum ac porttitor mattis, quam mi hendrerit ligula, non pellentesque enim mi dapibus ante. \n\
+                    Vestibulum nisl lectus, fermentum in auctor at, imperdiet nec nulla. \n\
+                    Fusce tellus libero, venenatis vitae neque eu, vehicula placerat tortor. Sed ultrices aliquam risus sed sodales.';
+}
